c4f016bd781ba780264db24f1cac67c002d56e09
Zichen Wang <wangzichen530@hotmail.com>
2016-12-31 12:13:40 -0600

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Project.create!(
+  name:"FavOOreD",
+  date:"March 2016 - June 2016",
+  area:"Product Design, UI/UX Design",
+  intro:"an app to help people make better choices when eating out",
+  details:"blahblahblah\nblah",
+  tech:"Photoshop"
+)
+
+Project.create!(
+  name:"API_GO",
+  date:"March 2016 - June 2016",
+  area:"Full Stack Development",
+  intro:"a central hub for APIs",
+  details:"blahblahblah\nblah",
+  tech:"Meteor"
+)
