92a5fe8d94558d4ea596430fc32cccbc8b577c55
Zichen Wang <wangzichen530@hotmail.com>
2016-12-31 22:39:01 -0600

    projects index page update

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,14 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  # get 'projects/index'
+  #
+  # get 'projects/show'
+  #
+  # get 'projects/new'
+  #
+  # get 'projects/edit'
+
+  resources :projects
+
+  root 'projects#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
