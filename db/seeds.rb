12a51980eb1659782fcf3c7aa50836c72f328dd0
Zichen Wang <wangzichen530@hotmail.com>
2017-01-04 22:22:49 -0600

    add more in about_me and projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Project.create!(
-  name:"FavOOreD",
-  date:"March 2016 - June 2016",
-  area:"Product Design, UI/UX Design",
-  intro:"an app to help people make better choices when eating out",
+  name:"Internship at Highrise Pet Care",
+  date:"June 2016 - Sep. 2016",
+  area:"Front End Development, Mobile Application Development, Agile Development",
+  intro:"Developed two collaborative mobile web applications using Ionic Framework and Firebase",
   details:"blahblahblah\nblah",
-  tech:"Photoshop"
+  tech:"Ionic Framework, AngularJS, HTML5, CSS, Firebase"
 )
 
 Project.create!(
@@ -24,34 +24,34 @@ Project.create!(
 )
 
 Project.create!(
-  name:"FavOOreD",
-  date:"March 2016 - June 2016",
-  area:"Product Design, UI/UX Design",
-  intro:"an app to help people make better choices when eating out",
+  name:"Loneliness Detecting through Mobile Sensing",
+  date:"Jan. 2016 - March 2016",
+  area:"Mobile Application Development",
+  intro:"",
   details:"blahblahblah\nblah",
-  tech:"Photoshop"
-)
-
-Project.create!(
-  name:"API_GO",
-  date:"March 2016 - June 2016",
-  area:"Full Stack Development",
-  intro:"a central hub for APIs",
-  details:"blahblahblah\nblah",
-  tech:"Meteor"
+  tech:"Android"
 )
 
 Project.create!(
   name:"FavOOreD",
   date:"March 2016 - June 2016",
   area:"Product Design, UI/UX Design",
-  intro:"an app to help people make better choices when eating out",
+  intro:"a mobile app to encourage people make healthy choices when eating out",
   details:"blahblahblah\nblah",
-  tech:"Photoshop"
+  tech:"Photoshop, Design, Business Model"
 )
 
 Project.create!(
-  name:"API_GO",
+  name:"My Routes",
+  date:"March 2016 - June 2016",
+  area:"Product Design, Mobile Application Development",
+  intro:"",
+  details:"blahblahblah\nblah",
+  tech:"Android"
+)
+
+Project.create!(
+  name:"Coming soon ...",
   date:"March 2016 - June 2016",
   area:"Full Stack Development",
   intro:"a central hub for APIs",
